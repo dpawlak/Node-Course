983672eaa96ea6a3fd3aea7e4486b36a2b59006d
Daniel Pawlak <daniel@Daniels-iMac.local>
2020-12-08 12:05:39 -0800

    Product-Delete and Deleting Cart Items

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,6 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product', {
         pageTitle: 'Add Product', 
         path: '/admin/add-product',
-        editing: false
     })
 }
 
@@ -13,7 +12,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description =req.body.description;
-    const product = new Product(title, imageUrl, description, price)
+    const product = new Product(null, title, imageUrl, description, price)
     product.save()
     res.redirect('/')
 }
@@ -28,6 +27,23 @@ exports.getEditProduct = (req, res, next) => {
     })
 }
 
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId
+    const updatedTitle = req.body.title
+    const updatedPrice = req.body.price
+    const updatedImageUrl = req.body.imageUrl
+    const updatedDesc = req.body.description
+    const updatedProduct = new Product(
+        prodId,
+        updatedTitle,
+        updatedImageUrl,
+        updatedDesc,
+        updatedPrice
+    )
+    updatedProduct.save()
+    res.redirect('/admin/products')
+}
+
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
         res.render('admin/products', {
@@ -36,4 +52,10 @@ exports.getProducts = (req, res, next) => {
             path: '/admin/products'
         })
     })
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId
+    Product.deleteById(prodId)
+    res.redirect('/admin/products')
 }
