bd85ae3120fbc84da461d73d6fdaee54f1e65bda
Daniel Pawlak <daniel@Daniels-iMac.local>
2020-12-08 17:47:04 -0800

    Section 9: Dynamic Routes & Advanced Models

--- models/cart.js
+++ models/cart.js
@@ -42,6 +42,9 @@ module.exports = class Cart {
             }
             const updatedCart = {...JSON.parse(fileContent)}
             const product = updatedCart.products.find(prod => prod.id === id)
+            if (!product) {
+                return
+            }
             const productQty = product.qty
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
             updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty
@@ -60,5 +63,15 @@ module.exports = class Cart {
             cb(cart);
             }
         });
+    }
+    static getProducts() {
+        fs.read(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent)
+            if (err) {
+                cb(null)    
+            } else {
+                cb(cart)
+            }
+        })
     }
 };
