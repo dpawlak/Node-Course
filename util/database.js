09a8914c883662ed177d417f70f8ac613cbf89d8
Daniel Pawlak <daniel@Daniels-iMac.local>
2020-12-14 16:33:27 -0800

    Fetching All Products with MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', '72156zl!q', {
-    dialect: 'mysql', 
-    host: 'localhost'
-})
+let _db
 
-module.exports = sequelize
+const mongoConnect = callback => {
+    MongoClient.connect('mongodb+srv://Daniel:maha6kali@cluster0.5jkca.mongodb.net/node-complete?retryWrites=true&w=majority')
+    .then(client => {
+        console.log("Connected!")
+        _db = client.db()
+        callback()
+    })
+    .catch(err => {
+        console.log(err)
+        throw err
+    })
+}
+
+const getDb = () => {
+    if (_db) {
+        return _db
+    }
+    throw 'No database found!'
+}
+
+exports.mongoConnect = mongoConnect 
+exports.getDb = getDb 
