e31e090515a81d3861aea5c81b55f9ccb179b376
Daniel Pawlak <daniel@Daniels-iMac.local>
2020-12-16 11:19:38 -0800

    Section 12: Working with NoSQL & Using MongoDB

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,17 +44,13 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
     req.user
         .getCart()
-        .then(cart => {
-            return cart
-                .getProducts()
-                .then(products => {
+        .then(products => {
+            
                     res.render('shop/cart', {
                         path: '/cart',
                         pageTitle: 'Your Cart',
                         products: products
                 })
-            })
-            .catch(err => console.log(err))
         })
         .catch(err => console.log(err))
 }
@@ -64,55 +60,18 @@ exports.postCart = (req, res, next) => {
     Product.findByPk(prodId)
         .then(product => {
             return req.user.addToCart(product)
-            })
+        })
         .then(result => {
             console.log(result)
+            res.redirect('/cart')
         })
         .catch(err => {console.log(err)})
-    // let fetchedCart
-    // let newQuantity = 1
-    // req.user
-    //     .getCart()
-    //     .then(cart => {
-    //         fetchedCart = cart;
-    //         return cart.getProducts({ where: { id: prodId } });
-    //     })
-    //     .then(products => {
-    //         let product
-    //         if (products.length > 0) {
-    //             product = products[0]
-    //         }
-            
-    //         if (product) {
-    //             const oldQuantity = product.cartItem.quantity
-    //             newQuantity = oldQuantity + 1
-    //             return product
-    //             }
-    //         return Product.findByPk(prodId)
-               
-    //         })
-    //         .then(product => {
-    //             return fetchedCart.addProduct(product, {
-    //                 through: {quantity: newQuantity}
-    //             })
-    //         })
-    //         .then(() => {
-    //             res.redirect('/cart')
-    //         })
-    //         .catch(err => console.log(err))     
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId
     req.user
-        .getCart()
-        .then(cart => {
-            return cart.getProducts({where: {id:prodId}})
-        })
-        .then(products => {
-            const product = products[0]
-            return product.cartItem.destroy()
-        })
+        .deleteItemFromCart(prodId)
         .then(result => {
             res.redirect('/cart')
         })
@@ -122,27 +81,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
     let fetchedCart
     req.user
-        .getCart()
-        .then(cart => {
-            fetchedCart = cart
-            return cart.getProducts()
-        })
-        .then(products => {
-            return req.user
-                .createOrder()
-                .then(order => {
-                    return order.addProducts(
-                        products.map(product => {
-                            product.orderItem = {quantity: product.cartItem.quantity}
-                            return product 
-                        })
-                    )
-                })
-                .catch(err => console.log(err))
-        })
-        .then(result => {
-            return fetchedCart.setProducts(null)
-        })
+        .addOrder()
         .then(result => {
             res.redirect('/orders')
         })
@@ -151,7 +90,7 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
     req.user
-        .getOrders({ include: ['products'] })
+        .getOrders()
         .then(orders => {
             res.render('shop/orders', {
                 path: '/orders',
